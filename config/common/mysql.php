021c127a7e2e5bb6a0d324884ab2d8e66594c01c
RedCatGirl <Galchonok.Galka@Gmail.com>
2021-10-08 22:20:07 +0300

    Added deleted file :)

---
+++ config/common/mysql.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/** @var array $params */
+
+use Yiisoft\Db\Connection\ConnectionInterface;
+use Yiisoft\Db\Mysql\Connection;
+
+return [
+//    ConnectionInterface::class => [
+//        'class' => Connection::class,
+//        '__construct()' => [
+//            'dsn' => $params['yiisoft/db-mysql']['dsn'],
+//        ],
+//        'setUsername()' => [$params['yiisoft/db-mysql']['username']],
+//        'setPassword()' => [$params['yiisoft/db-mysql']['password']],
+//    ],
+];
